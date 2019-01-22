a84a64353bd8aa869561f58201b02fd8607bdeb0
apatoliya <apatoliya@gmail.com>
2019-01-21 22:28:08 -0600

    all three components working as expected on proxy

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(proxy("/songs", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com/
 app.use(proxy("/filterSongsRock", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
 app.use(proxy("/filterSongsJazz", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
 app.use(proxy("/filterSongsPop", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
+app.use(proxy("/data", {target: 'http://friendscomponent.us-west-1.elasticbeanstalk.com/'}));
+
 
 
 
