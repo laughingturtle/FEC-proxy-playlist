41423968f0670d18cf54440c987841065de95632
apatoliya <apatoliya@gmail.com>
2019-01-22 10:29:32 -0600

    modified proxy routes for backend services

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(proxy("/songs", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com/'}));
-app.use(proxy("/filterSongsRock", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
-app.use(proxy("/filterSongsJazz", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
-app.use(proxy("/filterSongsPop", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
+app.use(proxy("/filterSongsRock", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com/'}));
+app.use(proxy("/filterSongsJazz", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com/'}));
+app.use(proxy("/filterSongsPop", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com/'}));
 app.use(proxy("/data", {target: 'http://friendscomponent.us-west-1.elasticbeanstalk.com/'}));
 
 
